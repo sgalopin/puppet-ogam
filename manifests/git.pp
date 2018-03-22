840255698d829714c92b49b69e03dd9b0ae3f941
Sylvain Galopin <sylvain.galopin@ign.fr>
2018-03-22 11:57:06 +0100

    Changing OGAM repo

--- manifests/git.pp
+++ manifests/git.pp
@@ -3,12 +3,12 @@ class ogam::git {
     file_line { 'gitlab':
       ensure => present,
       path   => '/etc/hosts',
-      line   => '172.28.99.2 gitlab.dockerforge.ign.fr',
+      line   => '192.30.253.112 gitlab.dockerforge.ign.fr',
     }->
     vcsrepo { $ogam::git_clone_directory:
         ensure   => latest,
         provider => git,
-        source   => 'http://gitlab.dockerforge.ign.fr/ogam/ogam.git',
+        source   => 'https://github.com/IGNF/ogam.git',
         revision => 'master',
     }
 }
