7c94eba4ec8d970ce10dcaec77f89eba2cff0f7e
Sylvain Galopin <sylvain.galopin@ign.fr>
2018-03-22 15:57:34 +0100

    Adding git-lfs

--- manifests/git.pp
+++ manifests/git.pp
@@ -10,5 +10,17 @@ class ogam::git {
         provider => git,
         source   => 'https://github.com/IGNF/ogam.git',
         revision => 'master',
+    }->
+    # Installation of git lfs
+    exec { [ "wget -O git-lfs.deb https://packagecloud.io/github/git-lfs/packages/debian/jessie/git-lfs_2.4.0_amd64.deb/download 2>&1",
+             "dpkg -i git-lfs.deb" ] :
+        path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+        cwd     => $ogam::tmp_directory,
+        unless  => "test -f /usr/bin/git-lfs",
+    }->
+    # https://tickets.puppetlabs.com/browse/MODULES-4442
+    exec { 'git lfs fetch --all':
+        path    => '/usr/bin:/usr/sbin:/bin',
+        cwd     => $ogam::git_clone_directory,
     }
 }
