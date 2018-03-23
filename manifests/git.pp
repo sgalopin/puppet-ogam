fa1ee537d9b7451ad8a82ff84112ae4f95f6c7a8
Sylvain Galopin <sylvain.galopin@ign.fr>
2018-03-23 10:01:25 +0100

    deletion of the --all for the git lfs fetch

--- manifests/git.pp
+++ manifests/git.pp
@@ -1,5 +1,13 @@
 class ogam::git {
 
+    # Installation of git lfs
+    # https://tickets.puppetlabs.com/browse/MODULES-4442
+    exec { [ "wget -O git-lfs.deb https://packagecloud.io/github/git-lfs/packages/debian/jessie/git-lfs_2.4.0_amd64.deb/download 2>&1",
+             "dpkg -i git-lfs.deb" ] :
+        path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+        cwd     => $ogam::tmp_directory,
+        unless  => "test -f /usr/bin/git-lfs",
+    }->
     file_line { 'gitlab':
       ensure => present,
       path   => '/etc/hosts',
@@ -10,17 +18,5 @@ class ogam::git {
         provider => git,
         source   => 'https://github.com/IGNF/ogam.git',
         revision => 'master',
-    }->
-    # Installation of git lfs
-    exec { [ "wget -O git-lfs.deb https://packagecloud.io/github/git-lfs/packages/debian/jessie/git-lfs_2.4.0_amd64.deb/download 2>&1",
-             "dpkg -i git-lfs.deb" ] :
-        path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-        cwd     => $ogam::tmp_directory,
-        unless  => "test -f /usr/bin/git-lfs",
-    }->
-    # https://tickets.puppetlabs.com/browse/MODULES-4442
-    exec { 'git lfs fetch --all':
-        path    => '/usr/bin:/usr/sbin:/bin',
-        cwd     => $ogam::git_clone_directory,
     }
 }
